75bc86bcaacfafdfe443a3be2a7517d21a792e66
christian <chrissantos1995@gmail.com>
2016-08-09 18:11:55 -0400

    Fixed bug where web.xml had incorrect info about HttpMovieRanker class

--- src/com/movie/ranker/HttpMovieRanker.java
+++ src/com/movie/ranker/HttpMovieRanker.java
@@ -9,28 +9,31 @@ import org.json.simple.JSONObject;
 
 // Extend HttpServlet class
 public class HttpMovieRanker extends HttpServlet {
- 
+
   private String message;
 
-  public void init() throws ServletException {
+  public void init() throws ServletException
+  {
       // Do required initialization
       message = "Hello World";
   }
 
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
-            throws ServletException, IOException {
-              
+            throws ServletException, IOException
+  {
       // Set response content type
       response.setContentType("text/html");
+
+      message = request.getParameter("name");
 
       // Actual logic goes here.
       PrintWriter out = response.getWriter();
       out.println("<h1>" + message + "</h1>");
   }
   
-  public void destroy() {
+  public void destroy()
+  {
       // do nothing.
   }
-
 }
